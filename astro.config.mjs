b2eebc24e863a211fa28399fbb9e86d4cec0120e
zrh-LegionWin <r9644360@gmail.com>
2026-03-07 22:32:56 +0800

    fix markdown rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,11 @@ export default defineConfig({
   image: {
     service: { entrypoint: 'astro/assets/services/noop' },
   },
+  markdown: {
+    shikiConfig: {
+      theme: 'one-light',
+    },
+  },
   vite: {
     plugins: [tailwindcss()],
   },
